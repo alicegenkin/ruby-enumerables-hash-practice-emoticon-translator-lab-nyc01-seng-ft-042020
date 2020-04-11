72f9cf8f863fdbe4b7a92f38ac0c2fd2c0667df5
Alice <agenkin1@gmail.com>
2020-04-11 17:58:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,17 @@
-# require modules here
-
-def load_library
+# require modules her
+def load_library(file_name)
+require 'yaml'
+  emoticons = YAML.load(File.read(file_name))
+  newEmoticons = {}
+  emoticons.each do |key,value|
+    newEmoticons[key] = {:english: value[0], :japanese: value[1]}
+end
+ #.map { |e| [e, { }] }
   # code goes here
+return newEmoticons
+end
+
+# code goes here
 end
 
 def get_japanese_emoticon
